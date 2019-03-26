e9b7fea332af676e8a9392ed454fd6220e4fa5a3
Vlas Dielov <Vlas.Dielov@ge.com>
2019-03-26 19:38:27 +0200

    cooperate requirejs and angular, also format app to dataFormatting

--- scripts/ngModules/ngModule1.js
+++ scripts/ngModules/ngModule1.js
@@ -1,9 +1,33 @@
-var ngModule1 = angular.module('ngModule1',[]);
-ngModule1.controller( 'mgModel1Ctrl', function( $scope ) {
-    $scope.someValue = 'some%Value%';
-});
-ngModule1.filter('deletePercents', function () {
-    return function (txt) {
-        return txt.replace(/%/, '');
+//
+define(['angular', 'jquery', 'moment'], function( angular, $, moment ) {
+        var ngModule1 = angular.module('ngModule1', []);
+        var parsingFormats = ["DD:MM:YYYY","YYYY:MM:DD","YYYY:MMM:DD"];
+
+        $('.jqueryChangeFont').css(['color', 'green']);
+
+        // Controller defines scope for html elements with this module used
+        ngModule1.controller('ngModule1Ctrl', function ($scope) {
+            $scope.someValue = 'some%Value%';
+            $scope.formats = ["DD + MMM + YYYY:hh+ZZ", "LLLL"];
+            $scope.locales = ["fr","en","us"];
+            $scope.momentOutput1 = function (txtDate, format, locale) {
+                // it parser array which looks for matching, it parses all non alphabetic letters like seperator : and another such as * // / etc
+
+                var moment_1 = moment(txtDate, parsingFormats, locale);
+
+                return moment_1.format(format);
+            };
+            $scope.momentOutput2 = function (txtDate, format, locale) {
+                // it parser array which looks for matching, it parses all non alphabetic letters like seperator : and another such as * // / etc
+
+                var moment_1 = moment(txtDate, parsingFormats, locale);
+
+                return moment_1.format(format);
+            };
+        });
+
+        // Filters used in html expression to pipe | some txt
+
+        return ngModule1;
     }
-});
+);
