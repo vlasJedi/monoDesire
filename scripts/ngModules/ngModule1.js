bd05780a9bc4cf9190e7429f40bc9dc45b6dfa40
vlasJedi <vlas.d78@gmail.com>
2019-04-02 23:31:49 +0300

    customization of date format and parsing

--- scripts/ngModules/ngModule1.js
+++ scripts/ngModules/ngModule1.js
@@ -8,21 +8,15 @@ define(['angular', 'jquery', 'moment'], function( angular, $, moment ) {
         // Controller defines scope for html elements with this module used
         ngModule1.controller('ngModule1Ctrl', function ($scope) {
             $scope.someValue = 'some%Value%';
-            $scope.formats = ["DD + MMM + YYYY:hh+ZZ", "LLLL"];
-            $scope.locales = ["fr","en","us"];
-            $scope.momentOutput1 = function (txtDate, format, locale) {
+            // some example of formats: LLLL - locales
+            //$scope.formats = ["DD + MMM + YYYY:hh+ZZ", "LLLL"];
+            //$scope.locales = ["fr","en","us"];
+            $scope.momentCustomOutput = function (txtDate, dateParseFormat, dateCustomFormat, locale) {
                 // it parser array which looks for matching, it parses all non alphabetic letters like seperator : and another such as * // / etc
 
-                var moment_1 = moment(txtDate, parsingFormats, locale);
+                var moment_1 = moment(txtDate, dateParseFormat, locale);
 
-                return moment_1.format(format);
-            };
-            $scope.momentOutput2 = function (txtDate, format, locale) {
-                // it parser array which looks for matching, it parses all non alphabetic letters like seperator : and another such as * // / etc
-
-                var moment_1 = moment(txtDate, parsingFormats, locale);
-
-                return moment_1.format(format);
+                return moment_1.format(dateCustomFormat);
             };
         });
 
