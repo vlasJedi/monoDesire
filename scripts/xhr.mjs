3aaa35f506e5452edf54195e286305830ae54390
vlas <vlas.d78@gmail.com>
2019-05-01 20:31:09 +0300

    created object xhr to use from client side request to server

--- scripts/xhr.mjs
+++ scripts/xhr.mjs
@@ -1,70 +1,155 @@
 export default function() {
     return {
-        setRequestParams: function(method, url, requestHeaders, urlSearchParams, body, callback) {
+        setRequestParams: function(method, url, requestHeaders, urlQueryParams, body, callbackSuccess, callbackFault) {
+
             var xhr = new XMLHttpRequest();
+            var urlQueryObj;
+            //xhr.withCredentials = true;
+            //xhr.timeout(time in ms);
+                /*
+                The XMLHttpRequest upload property returns an XMLHttpRequestUpload object that can be observed to
+                monitor an upload's progress. It is an opaque object, but because it's also an XMLHttpRequestEventTarget,
+                event listeners can be attached to track its process.
+                 */
+            //xhr.upload(data);
+
+            /*
+            var request = {
+                method: "GET",
+                url: "https://my-json-server.typicode.com/vlasJedi/RestAPI", //Same Origin Policy - by default only possible access to protocol://domain:port
+                asynch: true,
+                user: null,
+                password: null
+            }
+            xhr.open(request.method,request.url,request.asynch,request.user,request.password);  // initializates request
+             */
+
             xhr.open(method, url);
 
-            function setRequestHeaders(xht, requestHeaders) {
-                if (requestHeaders) {
-                    for (var key in requestHeaders) {
-                        if (requestHeaders.hasOwnProperty(key)) {
-                            xhr.setRequestHeader(key, requestHeaders[key]);
-                        }
+            //xhr.setRequestHeader("Access-Control-Allow-Origin","file:///C:/requireJS");
+
+            if (requestHeaders) {
+                setRequestHeaders(xhr, requestHeaders);
+            }
+
+            if (urlQueryParams) {
+                urlQueryObj = getUrlObjectForSend(urlQueryParams);
+            }
+
+            switch (method) {
+                // xhr.send('string');
+                /*No, Blob URLs/Object URLs can only be made internally in the browser. You can make Blobs and get File
+                    object via the File Reader API, although BLOB just means Binary Large OBject and is stored as
+                    byte-arrays. A client can request the data to be sent as either ArrayBuffer or as a Blob.
+                    The server should send the data as pure binary data. Databases often uses Blob to describe binary
+                    objects as well, and in essence we are talking basically about byte-arrays.*/
+                // xhr.send(new Blob());
+                // xhr.send(new Int8Array());
+                // xhr.send({ form: 'data' });
+                // xhr.send(document);
+                case 'get':
+                    xhr.send();
+                    break;
+                case 'post':
+                    xhr.send(body || urlQueryObj);
+                    break;
+            }
+
+            //xhr.status; // code
+                //
+            //xhr.responseType - types:
+                //" " treated as "text"
+                // "document" - HTML or XML doc
+                // "json"
+                // "text"
+            //xhr.readyState - types:
+                // 0 - UNSENT - xhr instance created
+                // 1 - OPENED - xhr.open() called
+                // 2 - HEADERS_RECEIVED - xhr.send() called, received headers and status from server
+                // 3 - LOADING - some partial data available
+                // 4 - DONE - operation done
+
+            //xhr.statusText; // text to code
+            //xhr.responseText; // response text from server
+            //xhr.responseXML; // response in XMl
+
+            //xhr.abort;
+
+            xhr.addEventListener('loadend', function () {
+                if (xhr.readyState === 4 ) {
+                    callbackSuccess(xhr.response);
+                    return;
+                }
+                callbackFault();
+            });
+
+            /**
+             * Set request's headers for xhr
+             *
+             * @param {object} xhr
+             * @param {object} requestHeaders -> with typeof <map> <headerName>: <headerValue>
+             * @return {void}
+             */
+            function setRequestHeaders(xhr, requestHeaders) {
+                for (var requestHeader in requestHeaders) {
+                    if (requestHeaders.hasOwnProperty(requestHeader)) {
+                        xhr.setRequestHeader(requestHeader, requestHeaders[requestHeader]);
                     }
                 }
             };
 
-            setRequestHeaders(xhr, requestHeaders);
-            if (urlSearchParams) {
-                var URLqueryObj = new URLSearchParams();
-                for( var key in query ) {
-                    //URLqueryObj.set( key , query[key] );
+            /**
+             * Uses embedded constructor for urlQuery < new urlSearchParams()> in JS
+             *
+             * @param {string || object} urlQueryParams -> string with params or object typeof <map>
+             * @return {URLSearchParams || undefined} constructed object
+             */
+            function getUrlObjectForSend (urlQueryParams) {
+                var urlQueryObj;
+                switch (typeof (urlQueryParams)) {
+                    case 'string':
+                        urlQueryObj = new URLSearchParams(urlQueryParams);
+                        break;
+                    case 'object':
+                        urlQueryObj = new URLSearchParams();
+                        Object.keys(urlQueryParams).forEach(function (urlQueryParamKey) {
+                            // set -> overrides old one param with new one
+                            // append -> should simply add even already param exists
+
+                            urlQueryObj.set(urlQueryParamKey, urlQueryParams[urlQueryParamKey]);
+                        });
+                        break;
                 }
-            }
-
+                return urlQueryObj;
+            };
 
         },
-        get: function (url, requestHeaders, urlSearchParams, callback) {
-            this.setRequestParams("get", url, requestHeaders, urlSearchParams, null, callback);
+        /**
+         * Calls a GET method
+         *
+         * @param {string} url
+         * @param {object || undefined || null} requestHeaders
+         * @param {function} callbackSuccess -> success callback function to get result of request
+         * @param {function} callbackFault -> fault callback function to get result of request
+         * @return {object}
+         */
+        get: function (url, requestHeaders, callbackSuccess, callbackFault) {
+            return this.setRequestParams("get", url, requestHeaders, null, null, callbackSuccess, callbackFault);
         },
-        post: function (url, requestHeaders, urlSearchParams, body, callback) {
-            this.setRequestParams("post", url, requestHeaders, urlSearchParams, body, callback);
+        /**
+         * Calls a POST method
+         *
+         * @param {string} url
+         * @param {object || undefined || null} requestHeaders
+         * @param {string || object} urlQueryParams -> sets in body when sends, but specific "body" like doc or string
+         *                                              will override the urlQueryString
+         * @param {document || string} body -> info to send
+         * @param {function} callbackSuccess -> success callback function to get result of request
+         * @param {function} callbackFault -> fault callback function to get result of request
+         * @return {object}
+         */
+        post: function (url, requestHeaders, urlQueryParams, body, callbackSuccess, callbackFault) {
+            return this.setRequestParams("post", url, requestHeaders, urlQueryParams, body, callbackSuccess, callbackFault);
         }
-        /*var xhr = new XMLHttpRequest();
-		//xhr.withCredentials = true;
-		var request = {
-			method: "GET",
-			url: "https://my-json-server.typicode.com/vlasJedi/RestAPI", //Same Origin Policy - by default only possible access to protocol://domain:port
-			asynch: true,
-			user: null,
-			password: null
-		}
-		xhr.open(request.method,request.url,request.asynch,request.user,request.password);  // initializates request
-		xhr.setRequestHeader("Access-Control-Allow-Origin","file:///C:/requireJS");
-		xhr.send(); // is async by default, but can be sync  -- as argument can take next
-		//xhr.status; // code
-		//xhr.statusText; // text to code
-		//xhr.responseText; // response text from server
-		//xhr.responseXML; // response in XMl
-		//xhr.abort;
-		//var query = {
-			//fname: "vlas",
-			//sname: "dielov"
-		//};
-		//var URLqueryObj = new URLSearchParams();
-		//for( var key in query ) {
-			//URLqueryObj.set( key , query[key] );
-		//}
-		//console.log(URLqueryObj.toString());
-		//xhr.send(URLqueryObj);
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState != 4 ) {
-				console.log("Current readyState value is: " + xhr.readyState );
-				return;
-			}
-			console.log("Server status code xhr.status: " + xhr.status + ", server status text: " + xhr.statusText );
-			console.log("Response from server xhr.responseText: " + xhr.responseText);
-
-		};*/
     };
 };
