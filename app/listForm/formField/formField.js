ee3f522e94a0fb0a1f8144cd54597abcf1a64488
vlas <vlas.d78@gmail.com>
2019-11-25 00:37:24 +0200

    started-with-form-fields-models-ctrls

--- app/listForm/formField/formField.js
+++ app/listForm/formField/formField.js
@@ -2,15 +2,23 @@ define(["angular", "text!formField.html"], function(angular, formField) {
     return function () {
         return {
             template: formField,
-            replace: true,
             scope: {
                 modelSync: "=",
-                handle: "=",
-                name: "@"
+                onChange: "&",
+                caption: "=",
+                ngModel: "=",
+                ngRequired: "@",
+                pattern: "="
             },
-            require: "^form",
-            link: function(scope, element, attrs, formCtrl) {
-
+            // specify directive to require
+            require: ["^form", "^listForm", "ngModel"], // to get form ctrl is default require with ^form
+            link: function(scope, element, attrs, formCtrls) {
+                let formCtrl = formCtrls[0];
+                let mainCtrl = formCtrls[1];
+                let thisInput = formCtrls[2];
+                element.on("click", function() {
+                   mainCtrl.notifyFormCtrl({element: scope, event: "click"});
+                });
             }
         };
     };
--- app/listForm/listForm.js
+++ app/listForm/listForm.js
@@ -8,8 +8,10 @@ define(['angular', "text!listForm.html", "formField"], function(angular, listFor
         return {
             template: listFormHTML,
             restrict: "E",
-            replace: true,
-            controller: ['$scope', function ($scope) {
+            // defines controller which can be accessed by scope props
+            // but also with this. methods which can be accessed when some child directive
+            // will need use parent shared ctrl to inter with others
+            controller: function ($scope, $element, $attrs) {
                 $scope.models = [
                     {
                         formFieldName: "fName",
@@ -25,12 +27,28 @@ define(['angular', "text!listForm.html", "formField"], function(angular, listFor
                     }
                 ];
                 $scope.handle = {
-                    onChangeFormField: function(textDisplay) {
-                        console.log(textDisplay);
+                    onChangeFormField: function(textDisplay, name) {
+                        console.log(`Form field input value has changed due to user interaction to: ${textDisplay}`);
+                        // VERY IMPORTANT ! HERE IS AVAILABLE FORM CTRL
+                        let input = $scope.mainCtrl.rootListForm[name];
+                        console.log(`Input ${input.$name} has $viewValue: ${input.$inputValue}`);
+                        console.log(`Input ${input.$name} has $invalid: ${input.$invalid}`);
+                        console.log(`Input ${input.$name} has $touched: ${input.$touched}`);
+                        console.log(`Input ${input.$name} has $dirty: ${input.$dirty}`);
+                        console.log(`Input ${input.$name} has $pristine: ${input.$pristine}`);
+                        console.log(`Input ${input.$name} has $valid: ${input.$valid}`);
+                        console.log(`Input ${input.$name} has $untouched: ${input.$untouched}`);
+                        console.log(`Input ${input.$name} has $pending: ${input.$pending}`);
                     }
                 };
-            }],
-            controllerAs: "listFormCtrl"
+                // this is accessible via require the directive
+                this.notifyFormCtrl = function(elementUpdate) {
+                    console.log(`Input field with name: ${elementUpdate.element.modelSync.formFieldName} + has such event: ${elementUpdate.event}`);
+                }
+            },
+            // ctrl this. can be accessed via controllerAs value in the directive,
+            //
+            controllerAs: "mainCtrl"
         };
     });
 });
